6b1c8204e3c0bbae2aaf866db415cc7f6d6993d5
Farelkin <farelka.vanya@yandex.ru>
2018-11-16 21:04:01 +0300

    add index.html

--- HW 7/rsync-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BACKUP_SRC=/srv/share/
-BACKUP_DST=/backup/
-CUR_DATE=`date '+%d-%m-%Y'`
-RSYNC_BIN=/usr/bin/rsync
-
-$RSYNC_BIN -a --log-file=/var/log/backup_share.log --stats --progress -rb --backup-dir=${BACKUP_DST}${CUR_DATE}/ ${BACKUP_SRC} ${BACKUP_DST}FULL/
